a9f16e3cb73aca5274829bc755998740e0cb641c
Vinicius Machado <vinicius.machado@3aworldwide.com.br>
2019-12-10 17:28:39 -0300

    site finish

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,34 @@ $(document).ready(function () {
     var title_three = $('.slider_home .item .title_three');
 
     $('.slider_home').slick({
-
-    }).on('beforeChange', function (event, slick, currentSlide, nextSlide, prevSlide) {
-        title_one.removeClass('animated fadeInDown');
-        title_one.addClass('animated fadeInDown');
-        console.log('entrou');
+        autoplay: true,
+        autoplaySpeed: 3500,
+        speed: 1500,
 
     });
+    $('.slider_contact').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        speed: 1500,
+    });
+    $(".bar").click(function () {
+        $("#menu_bar").addClass('active');
+        $("#mask").addClass('active');
+    });
+    $("#mask").click(function () {
+        $("#menu_bar").removeClass('active');
+        $("#mask").removeClass('active');
+    });
+    $(".close").click(function () {
+        $("#menu_bar").removeClass('active');
+        $("#mask").removeClass('active');
+    });
+    $(".bar_mobile").click(function () {
+        $(".menu").addClass('active');
+    });
+    $(".close_menu").click(function () {
+        $(".menu").removeClass('active');
+    });
+
 });
